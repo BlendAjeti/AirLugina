90e676bf551e35d717def19cdb20f77c18cfbfe1
BlendAjeti <blend.ajeti@hotmail.com>
2025-01-11 19:13:55 +0100

    shtyje

---
+++ assets/dropdown.js
@@ -0,0 +1,14 @@
+const userCtn = document.querySelector('.user-ctn');
+const userLogo = document.querySelector('.user-logo');
+const userCtnn = document.querySelector('.user-ctnn');
+
+userCtn.addEventListener('click', function() {
+    
+    if (userLogo.style.display === 'none' || userLogo.style.display === '') {
+        userLogo.style.display = 'flex';
+    } else {
+        userLogo.style.display = 'none';
+    }
+
+    userCtnn.classList.toggle('active');
+});
