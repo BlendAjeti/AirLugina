c5bad4beac03b25f56804d84ba5ad570493d3237
Mete Birvenik <mekro112@gmail.com>
2025-01-31 14:15:18 +0100

    signup me php

--- signup.php
+++ signup.php
@@ -1,67 +1,111 @@
+<?php
+$message = "";
+$toastClass = "";
+$conn = new mysqli('localhost', 'root', '', 'database');
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $email = $_POST['Email'];
+    $password = $_POST['Password'];
+
+    $checkEmailStmt = $conn->prepare("SELECT email FROM userdata WHERE email = ?");
+    $checkEmailStmt->bind_param("s", $email);
+    $checkEmailStmt->execute();
+    $checkEmailStmt->store_result();
+
+    if ($checkEmailStmt->num_rows > 0) {
+        $message = "Email ID already exists";
+        echo "<script>alert('$message');</script>";
+    } else {
+        $stmt = $conn->prepare("INSERT INTO userdata (username, email, password) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $username, $email, $password);
+
+        if ($stmt->execute()) {
+
+            setcookie("username", $username, time() + (86400 * 30), "/");
+            setcookie("email", $email, time() + (86400 * 30), "/");
+
+            header("Location: landingpage.php");
+            exit();
+        } else {
+            $message = "Error: " . $stmt->error;
+            echo "<script>alert('$message');</script>";
+        }
+
+        $stmt->close();
+    }
+
+    $checkEmailStmt->close();
+    $conn->close();
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign Up</title>
-    <link rel="stylesheet" href="/AirLugina/assets/signupfinal.css">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="Assets/new-signup.css">
+    <title>SignUp</title>
 </head>
-<body>
+
+<body class="bg-light">
     <div class="container">
-        <img class="AirLugina" src="/AirLugina/assets/Images/AirLugina.png" alt="logo">
-        <img src="/AirLugina/assets/Images/Rectangle_20.png" alt="rectangle">
-        <br><br>
-        <h1>Sign up</h1>
-        <p class="p1">Let's get you all st up so you can access your personal account.</p>
-        <div class="table">
-            <form id="signupForm" onsubmit="return validateForm()">
-                <div class="inputbox" id="box1">
-                    <fieldset class="fieldset">
-                        <legend>First Name</legend>
-                        <input type="text" placeholder="First Name" data-validation="name" id="nameField">
-                    </fieldset>
+        <div class="img">
+            <img src="assets/Images/forgot.png" alt="image">
+        </div>
+        <div class="forgot-ctn">
+            <div class="forgot">
+
+                <div class="logo">
+                    <img src="assets/Images/Air-Lugina-Logo.png" alt="AirLugina-Logo">
                 </div>
-                <div class="inputbox" id="box2">
-                    <fieldset class="fieldset">
-                        <legend>Last Name</legend>
-                        <input type="text" placeholder="Last Name" data-validation="surname" id="surnameField">
-                    </fieldset>
+                <div class="link">
+                    <a href="booking.php"><img src=" assets/Images/Vector.png" alt="vector"> Back to flights</a>
+                    <h1>Sign up</h1>
+                    <p>Let’s get you all set up so you can access your personal account.</p>
                 </div>
-                <div class="inputbox">
-                    <fieldset class="fieldset">
-                        <legend>Email</legend>
-                        <input type="email" placeholder="name@email.com" data-validation="email">
-                    </fieldset>
+                <div class="form-container">
+                    <form method="POST" id="signupForm">
+                        <div class="name-ctn">
+                            <fieldset style="width: 100%;" class="fieldset">
+                                <legend>Username</legend>
+                                <input type="text" name="username" id="username" placeholder="username">
+                            </fieldset>
+                        </div>
+                        <fieldset class="fieldset">
+                            <legend>Email</legend>
+                            <input type="email" placeholder="name@email.com" data-validation="email" name="Email">
+                        </fieldset>
+                        <fieldset class="fieldset">
+                            <legend>Password</legend>
+                            <input type="password" placeholder="Password" data-validation="password" id="passwordField" name="Password">
+                        </fieldset>
+                        <fieldset class="fieldset">
+                            <legend>Confirm Password</legend>
+                            <input type="password" placeholder="Confirm Password" data-validation="confirmPassword" id="confirmPasswordField">
+                        </fieldset>
+                        <div class="remember">
+                            <div class="input">
+                                <input type="checkbox" id="terms">
+                            </div>
+                            <div class="p">
+                                <p>I agree to all the <span>Terms</span> and <span>Privacy Policies</span></p>
+                            </div>
+                        </div>
+                        <button type="submit">Create account</button>
+                        <div class="register">
+                            <p>Already have an account?
+                                <a href="login.php">Login</a>
+                            </p>
+                        </div>
+                    </form>
                 </div>
-                <div class="inputbox">
-                    <fieldset class="fieldset">
-                        <legend>Password</legend>
-                        <input type="password" placeholder="Password" data-validation="password" id="passwordField">
-                        <i class="bx bx-hide toggle-password" id="togglePassword"></i>
-                    </fieldset>
-                </div>
-                <div class="inputbox">
-                    <fieldset class="fieldset">
-                        <legend>Confirm Password</legend>
-                        <input type="password" placeholder="Confirm Password" data-validation="confirmPassword" id="confirmPasswordField">
-                        <i class="bx bx-hide toggle-password" id="togglePassword"></i>
-                    </fieldset>
-                </div>
-                <div class="remember">
-                    <label>
-                        <input type="checkbox" id="terms"><p> I agree to all the Terms and Privacy Policies</p>
-                    </label>
-                </div>
-                <button type="submit">Create account</button>
-                <div class="register">
-                    <p>Already have an account? 
-                        <a href="/AirLugina/login.html">Login</a>
-                    </p>
-                </div>
-            </form>
+            </div>
+
         </div>
     </div>
-    <script src="signup.js"></script>
-</body>
+    <script src="assets/signup.js"></script>
+
 </html>
