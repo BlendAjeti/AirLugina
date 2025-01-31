2b93c04ddf363a6cc64af28f9d3f5c228a9bf7f4
FocusedStudio <160801303+FocusedStudio@users.noreply.github.com>
2025-01-31 14:21:05 +0100

    Editimi dhe implementrimi i footer

---
+++ footer.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/footer.css">
+
+</head>
+
+<div class="footer">
+    <div class="lugina-logo">
+        <div class="air-img">
+            <img src=" Assets/Images/AirLugina-footer.png" alt="Air-Lugina">
+        </div>
+        <div class="social-medias">
+            <img src=" Assets/Images/Social-medias.png" alt="Social-Medias">
+        </div>
+        <div>
+            <p>Made with ❤️ by Blendi | Mete | Engji</p>
+        </div>
+    </div>
+</div>
+
+<script src="assets/menu-drop.js"></script>
+<script src="assets/dropdown.js"></script>
+<!-- <script src="assets/toggle.js"></script> -->
+
+</body>
+
+</html>
