a4999cf9a6e6132ab0631e99f957ba7c39c6e339
Blend Ajeti <116169382+BlendAjeti@users.noreply.github.com>
2025-01-31 14:35:54 +0100

    Pushing dropdown

--- assets/dropdown.js
+++ assets/dropdown.js
@@ -1,14 +1,14 @@
-const userCtn = document.querySelector('.user-ctn');
+const userCtn = document.querySelector('.user-ctnn');
 const userLogo = document.querySelector('.user-logo');
 const userCtnn = document.querySelector('.user-ctnn');
 
-userCtn.addEventListener('click', function() {
-    
-    if (userLogo.style.display === 'none' || userLogo.style.display === '') {
-        userLogo.style.display = 'flex';
-    } else {
-        userLogo.style.display = 'none';
-    }
+userCtn.addEventListener('click', function () {
+  if (userLogo.style.display === 'none' || userLogo.style.display === '') {
+    userLogo.style.display = 'flex';
+  } else {
+    userLogo.style.display = 'none';
+  }
 
-    userCtnn.classList.toggle('active');
+  userCtnn.classList.toggle('active');
 });
+
